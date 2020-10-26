cbfa1baa9605e2fc2b2465898bf5a2412650e77a
Rick <1358735+rickb777@users.noreply.github.com>
2020-10-26 08:30:09 +0000

    documentation and small tweaks

--- negotiate.go
+++ negotiate.go
@@ -105,15 +105,21 @@ func (n *Negotiator) Negotiate(w http.ResponseWriter, req *http.Request, offers 
 	//charsets := header.Parse(req.Header.Get(AcceptCharset)).WithDefault()
 	//encodings := header.Parse(req.Header.Get(AcceptEncoding)).WithDefault()
 
+	// The first pass checks for exact matches, and this includes exclusions (q=0)
+	// The second pass allows wildcard matching.
 	for pass := 1; pass <= 2; pass++ {
-		for _, accepted := range mrs {
-			for _, lang := range languages {
-				for _, offer := range offers {
+		allowWildcard := pass == 2
+
+		for _, offer := range offers {
+			offeredType, offeredSubtype := split(offer.MediaType, '/')
+
+			for _, accepted := range mrs {
+				for _, lang := range languages {
 					n.info("200 compared", accepted.Value(), lang.Value, offer)
-					t, s := split(offer.MediaType, '/')
-					if equalOrWildcard(accepted.Type, t, pass == 2) &&
-						equalOrWildcard(accepted.Subtype, s, pass == 2) &&
-						equalOrWildcard(lang.Value, offer.Language, pass == 2) {
+
+					if equalOrWildcard(accepted.Type, offeredType, allowWildcard) &&
+						equalOrWildcard(accepted.Subtype, offeredSubtype, allowWildcard) &&
+						equalOrWildcard(lang.Value, offer.Language, allowWildcard) {
 
 						if accepted.Quality > 0 && lang.Quality > 0 {
 							for _, processor := range n.processors {
@@ -165,63 +171,6 @@ func (n *Negotiator) ajaxNegotiate(w http.ResponseWriter, req *http.Request, off
 
 	return n.notAcceptable(w)
 }
-
-// Firstly, all Ajax requests are processed by the first available Ajax processor.
-// Otherwise, standard content negotiation kicks in.
-//
-// A request without any Accept header field implies that the user agent
-// will accept any media type in response.
-//
-// If the header field is present in a request and none of the available
-// representations for the response have a media type that is listed as
-// acceptable, the origin server can either honour the header field by
-// sending a 406 (Not Acceptable) response or disregard the header field
-// by treating the response as if it is not subject to content negotiation.
-// This implementation prefers the former.
-//
-// See rfc7231-sec5.3.2:
-// http://tools.ietf.org/html/rfc7231#section-5.3.2
-//func (n *Negotiator) negotiateHeader(w http.ResponseWriter, req *http.Request, dataModel interface{}, template string) error {
-//	if fn, ok := dataModel.(func() interface{}); ok {
-//		dataModel = fn()
-//	}
-//
-//	if IsAjax(req) {
-//		for _, processor := range n.processors {
-//			ajax, doesAjax := processor.(AjaxResponseProcessor)
-//			if doesAjax && ajax.IsAjaxResponder() {
-//				return processor.Process(w, req, dataModel, template)
-//			}
-//		}
-//	}
-//
-//	if len(n.processors) > 0 {
-//		mrs := header.ParseMediaRanges(req.Header.Get(Accept)).WithDefault()
-//		charsets := header.Parse(req.Header.Get(AcceptCharset)).WithDefault()
-//		languages := header.Parse(req.Header.Get(AcceptLanguage)).WithDefault()
-//
-//		for _, accepted := range mrs {
-//			if accepted.Quality > 0 {
-//				for _, cs := range charsets {
-//					if cs.Quality > 0 {
-//						for _, lang := range languages {
-//							if lang.Quality > 0 {
-//								for _, processor := range n.processors {
-//									if processor.CanProcess(accepted.Value(), lang.Value) {
-//										return processor.Process(w, req, dataModel, template)
-//									}
-//								}
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//	}
-//
-//	n.errorHandler(w, "", http.StatusNotAcceptable)
-//	return nil
-//}
 
 // IsAjax tests whether a request has the Ajax header sent by browsers for XHR requests.
 func IsAjax(req *http.Request) bool {
